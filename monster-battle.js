8c9743629899d9df94de3ef638dcfcdbc96068ba
Mark Williams <mark@readysetmark.com>
2012-07-15 21:48:15 -0500

    Player status and attack options.

--- monster-battle.js
+++ monster-battle.js
@@ -1,22 +1,79 @@
+function method(object, name) {
+  return function() {
+    return object[name].apply(object, arguments);
+  };
+}
+
+
+
+function Player() {
+  this.health = 30;
+  this.agility = 30;
+  this.strength = 30;
+  this.attacks = {
+    "s": "[s]tab",
+    "d": "[d]ouble swing",
+    "r": "[r]oundhouse"
+  }
+}
+Player.prototype.isDead = function() {
+  return this.health <= 0;
+};
+Player.prototype.attackOptions = function() {
+  var attacks = "Select ttack style: "
+  for(var attack in this.attacks) {
+    attacks = attacks + " " + this.attacks[attack];
+  }
+  return attacks;
+}
+Player.prototype.status = function() {
+  return "You are a valiant knight with a health of " + this.health
+    + ", an agility of " + this.agility + ", and a strength of " + this.strength + "."
+};
+
+
 
 function MonsterBattle(place) {
+  // jQuery Console
   this.console = $('<div class="console">');
   place.append(this.console);
 
   this.controller = this.console.console({
     promptLabel: '> ',
-    commandValidate: function(line) {
-      if (line == "") return false;
-      else return true;
-    },
-    commandHandle: function(line) {
-      return [{msg: "Not implemented yet.",
-               className: "not-implemented"}]
-    },
+    //commandValidate: method(this, "commandValidate"),
+    commandHandle: method(this, "commandHandle"),
     autofocus: true,
     animateScroll: true,
     promptHistory: true,
+    welcomeMessage: 'Welcome to Monster Battle! Press <enter> to begin.'
     //charInsertTrigger: function(keycode,line) {}
   });
-}
 
+  // Game state
+  //this.gameState = null;
+}
+// MonsterBattle.prototype.commandValidate = function(line) {
+//   if (line == "") return false;
+//   else return true;
+// };
+MonsterBattle.prototype.commandHandle = function(line) {
+  if (this.gameState == null) {
+    return this.newGame();
+  }
+  else {
+    return [{msg: this.gameState.player.toString(),
+           className: "jquery-console-normal"}];
+  }
+};
+MonsterBattle.prototype.newGame = function() {
+  this.gameState = {
+    player: new Player()
+  }
+
+  return [
+    {msg: this.gameState.player.status(),
+      className: "jquery-console-player-status"},
+    {msg: this.gameState.player.attackOptions(),
+      className: "jquery-console-options"}
+  ];
+};
